ccbe2b885f1264c28d80590fe141ed38d3ea98e2
NeilFormosa <neilformosa6@gmail.com>
2024-12-18 11:09:25 +0100

    Add files via upload

---
+++ MainProgramCode.java
@@ -0,0 +1,21 @@
+
+import java.awt.Menu;
+import java.time.ZoneId;
+import javax.swing.*;
+import javax.xml.stream.events.StartElement;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Neil
+ */
+public class MainProgramCode {
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+        menu.setVisible(true);
+    }
+}
